5fa7e3d683b0229189b8e3103c851c42000aa950
ZoZo-182 <capzstream@gmail.com>
2026-08-30 21:23:52 -0400

    add yassification for dev

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["**/coverage/**", "**/dist/**", "**/node_modules/**"]),
+  {
+    files: ["apps/**/*.{js,mjs,cjs,ts,tsx}"],
+    extends: [js.configs.recommended],
+  },
+  {
+    files: ["apps/**/*.{ts,tsx}"],
+    extends: [tseslint.configs.recommended],
+  },
+  {
+    files: ["apps/api/src/**/*.ts"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  eslintConfigPrettier,
+]);
